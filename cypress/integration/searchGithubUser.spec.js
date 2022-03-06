c62e934898aec847ca43cfe8a7e2697d640bb90c
paupaugoz <37976584+paupaugoz@users.noreply.github.com>
2022-03-05 18:07:20 -0800

    Add cypress test for facebook use case

--- cypress/integration/searchGithubUser.spec.js
+++ cypress/integration/searchGithubUser.spec.js
@@ -1,4 +1,5 @@
 describe('Searching for a Github User', () => {
+    // Tests may be flaky without using cy.intercept as it does not wait for the value
     it('should be able to find my Github profile, and return a list of repositories', () => {
         cy.visit('/');
         cy.findByText('Search for a Github User...').should('exist').click({ force: true });
@@ -7,5 +8,19 @@ describe('Searching for a Github User', () => {
         cy.findByText('paupaugoz').should('exist').click();
 
         cy.findByText('paupaugoz/ruggable-study').should('exist');
+    });
+
+    it('should be able to find all repositories owned by Facebook sorted by most number of stars', () => {
+        cy.visit('/');
+        // cy.findByText('Search for a Github User...').should('exist').click({ force: true });
+        cy.get("[type='text']").type('facebook');
+
+        cy.findByText('facebook').should('exist').click();
+
+        // First list child should contain facebook/react
+        cy.get('li').eq(0).contains('facebook/react').should('exist');
+
+       // Second list child should contain facebook/react-native
+        cy.get('li').eq(1).contains('facebook/react-native').should('exist');
     });
 });
